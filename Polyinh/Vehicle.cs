8bcfb5b30c3b6c8e4e127bbd5d6e7641d30c47b8
Gavin Smith <no reply>
2026-03-17 10:54:40 +0000

    vehicle class created

---
+++ Polyinh/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyinh
+{
+    public abstract class Vehicle
+    {
+        public string Make { get; private set; } 
+        public string Model { get; private set; }
+        public int Speed { get; private set; }
+
+        public  Vehicle(string make, string model, int speed) 
+        {
+            Make = make;
+            Model = model;
+            Speed = speed;
+        }
+    }
+}

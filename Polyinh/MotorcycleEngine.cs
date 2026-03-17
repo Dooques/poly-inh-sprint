96e0368e693db9e684981f37c16c1d320c717958
Gavin Smith <no reply>
2026-03-17 11:30:08 +0000

    added engine types

---
+++ Polyinh/MotorcycleEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyinh
+{       
+    internal class MotorcycleEngine:Engine
+    {
+        public MotorcycleEngine(FuelType fuelType = FuelType.Leaded, int horsepower = 100):base(fuelType, horsepower)
+        {
+
+        }
+    }
+}

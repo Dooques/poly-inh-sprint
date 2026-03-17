e8e5f937cb3a36299a78d967fc2790796995b82e
Gavin Smith <no reply>
2026-03-17 11:05:00 +0000

    added motorcycle class

---
+++ Polyinh/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyinh
+{
+    internal class Motorcycle : Vehicle
+    {
+        public bool HasSideCar { get; set; }
+        public Motorcycle(string name, string model, int speed, bool hasSideCar ) : base(name, model, speed)
+        {
+            HasSideCar = hasSideCar;
+        }
+
+
+    }
+}
